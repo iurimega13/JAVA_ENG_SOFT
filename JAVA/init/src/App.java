4e2e5cde5d979c3f4857535985fdd8e8f8b5baa9
Iuri Galdino <iurimega13@gmail.com>
2023-10-12 12:08:27 -0300

    Add comentarios on code

--- JAVA/init/src/App.java
+++ JAVA/init/src/App.java
@@ -1,3 +1,10 @@
+/** 
+ * Codigo inicial para o curso de Java.
+ * @author Iuri de Araujo
+ * @version 1.0
+ * @since 2021-03-01
+ */
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
